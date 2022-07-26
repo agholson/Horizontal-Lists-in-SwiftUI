fa26f64b9385ca3afeb165d2e46f8539162a337a
agholson <87455899+agholson@users.noreply.github.com>
2022-07-26 16:37:54 -0400

    Initial Commit

---
+++ HorizontalListsL6/HorizontalListsL6App.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalListsL6App.swift
+//  HorizontalListsL6
+//
+//  Created by Leone on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalListsL6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
